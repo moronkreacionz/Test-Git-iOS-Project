8ba45ca084e9ac0d84a036b4202d18aeabf25d68
moronkreacionz <moronkreacionz@gmail.com>
2015-01-07 19:53:38 +0530

    Initial Commit

---
+++ Git-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git-Test
+//
+//  Created by Vijay Mehta on 1/7/15.
+//  Copyright (c) 2015 Vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git-Test
+//
+//  Created by Vijay Mehta on 1/7/15.
+//  Copyright (c) 2015 Vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
