7fc7809d97f2959efba26d7994fb03af73682182
moronkreacionz <moronkreacionz@gmail.com>
2015-01-07 21:00:51 +0530

    second commit added VjTest class

---
+++ Git-Test/VjTest.h
@@ -0,0 +1,13 @@
+//
+//  VjTest.h
+//  Git-Test
+//
+//  Created by Vijay Mehta on 1/7/15.
+//  Copyright (c) 2015 Vj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VjTest : NSObject
+
+@end
---
+++ Git-Test/VjTest.m
@@ -0,0 +1,13 @@
+//
+//  VjTest.m
+//  Git-Test
+//
+//  Created by Vijay Mehta on 1/7/15.
+//  Copyright (c) 2015 Vj. All rights reserved.
+//
+
+#import "VjTest.h"
+
+@implementation VjTest
+
+@end
